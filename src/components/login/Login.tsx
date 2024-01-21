8fcec8c468f6095b92d8308cc2a12950ebcebf1c
LauraEsmoris <lesmoris0web@gmail.com>
2024-01-20 22:57:12 -0500

    login

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,17 @@
+export function Login() {
+   return(
+      <div>
+         <span>login</span>
+         <div>
+            <label>username</label>
+            <input type={"text"}/>
+         </div>
+         <span>password</span>
+         <div>
+            <label>password</label>
+            <input type={"text"}/>
+         </div>
+         <button type={"submit"}>submit</button>
+      </div>
+   )
+}
